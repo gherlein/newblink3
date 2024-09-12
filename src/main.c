83086f81d4e41e33b9479668dbf05dc83e607908
Greg Herlein <gherlein@herlein.com>
2024-09-12 12:53:04 -0700

    updated

--- src/main.c
+++ src/main.c
@@ -8,7 +8,14 @@
 #include "pico/cyw43_arch.h"
 
 int main() {
-    stdio_init_all();
+   // workaround for a hardware debug problem
+    // https://forums.raspberrypi.com/viewtopic.php?t=363914
+    // also in openocd you need to us -c "set USE_CORE 0"  before rp2040.cfg is loaded per https://github.com/raspberrypi/debugprobe/issues/45
+    timer_hw->dbgpause = 0;
+    sleep_ms(150);
+
+
+  stdio_init_all();
     if (cyw43_arch_init()) {
         printf("Wi-Fi init failed");
         return -1;
